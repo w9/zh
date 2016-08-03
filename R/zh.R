dd1981e13cdacfc6dc96324ade5fa3512d4a5008
Xun Zhu <zhuxun2@gmail.com>
2016-08-02 14:43:54 -1000

    commit

--- R/zh.R
+++ R/zh.R
@@ -32,46 +32,100 @@
 # asdfasf -----------------------------------------------------------------
 
 zh <-
-  function(mat,
-           preserve_order=T,
-           use_viewer=F,
-           display_rownames=T,
-           display_colnames=T,
-           pixel_width=4,
-           pixel_height=4) {
-  col_mapper <- col_numeric(c('#132B43', '#56B1F7'), c(min(mat), max(mat)))
-  
-  d_names_list <- dimnames(mat)
-  d_names <- names(d_names_list)
-  row_names <- d_names_list[[1]]
-  col_names <- d_names_list[[2]]
-  
-  heatmap_64 <- mat %>%
-    col_mapper %>%
-    col2rgb %>%
-    divide_by(255) %>%
-    array(c(3, nrow(mat), ncol(mat))) %>%
-    aperm(c(2,3,1)) %>%
-    writePNG %>%
-    base64Encode %>%
-    sprintf('data:image/png;base64,%s', .)
-  
-  sizing_policy <- sizingPolicy(padding=0, browser.fill=T, viewer.suppress=!use_viewer)
-  
-  msg <- list()
-  msg$base64 <- heatmap_64
-  msg$rownames <- rownames(mat)
-  msg$colnames <- colnames(mat)
-  msg$nrow <- nrow(mat)
-  msg$ncol <- ncol(mat)
-  msg$display_rownames <- display_rownames
-  msg$display_colnames <- display_colnames
-  msg$pixel_width <- pixel_width
-  msg$pixel_height <- pixel_height
-  
-  createWidget('zh', msg, sizingPolicy=sizing_policy)
-}
+  function(
+    mat,
+    preserve_order            = T,
+    use_viewer                = F,
+    display_rownames          = T,
+    display_colnames          = T,
+    label_margin              = 10,
+    pixel_width_with_no_text  = 4,
+    pixel_height_with_no_text = 4,
+    pixel_width_with_text     = 15,
+    pixel_height_with_text    = 15
+    )
+  {
+    col_mapper <- col_numeric(c('#132B43', '#56B1F7'), c(min(mat), max(mat)))
+    
+    d_names_list <- dimnames(mat)
+    d_names <- names(d_names_list)
+    row_names <- d_names_list[[1]]
+    col_names <- d_names_list[[2]]
+    
+    heatmap_64 <- mat %>%
+      col_mapper %>%
+      col2rgb %>%
+      divide_by(255) %>%
+      array(c(3, nrow(mat), ncol(mat))) %>%
+      aperm(c(2,3,1)) %>%
+      writePNG %>%
+      base64Encode %>%
+      sprintf('data:image/png;base64,%s', .)
+    
+    pixel_width <- ifelse(display_rownames, pixel_width_with_text, pixel_width_with_no_text)
+    pixel_height <- ifelse(display_rownames, pixel_height_with_text, pixel_height_with_no_text)
 
+    svg <- read_xml('<svg id="zh-svg" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" />')
+    img <- read_xml('<image />')
+    xml_attr(img, 'id') <- 'zh-svg'
+    xml_attr(img, 'href') <- heatmap_64
+    xml_attr(img, 'height') <- (nrow(mat) * pixel_height) %>% as.character
+    xml_attr(img, 'width') <- (ncol(mat) * pixel_width) %>% as.character
+    xml_attr(img, 'x') <- 0 %>% as.character
+    xml_attr(img, 'y') <- 0 %>% as.character
+    xml_attr(img, 'preserveAspectRatio') <- 'none'
+    xml_attr(img, 'image-rendering') <- 'pixelated'
+    xml_add_child(svg, img)
+
+    rownames <- rownames(mat)
+    colnames <- colnames(mat)
+    nrow <- nrow(mat)
+    ncol <- ncol(mat)
+
+    g_rownames <- read_xml('<g />')
+    if (display_rownames && !is.null(rownames)) {
+      for (i in 1:length(rownames)) {
+        text <- read_xml('<text />')
+        xml_attr(text, 'id') <- 'zh-svg'
+        xml_attr(text, 'x') <- (- label_margin) %>% as.character
+        xml_attr(text, 'y') <- ((i - 1 + 0.5) * pixel_height_with_text) %>% as.character
+        xml_attr(text, 'class') <- 'zh-label'
+        xml_text(text) <- rownames[i]
+        xml_add_child(g_rownames, text)
+      }
+    }
+    xml_add_child(svg, g_rownames)
+
+    g_colnames <- read_xml('<g class="zh-rotate" />')
+    if (display_colnames && !is.null(colnames)) {
+      for (i in 1:length(colnames)) {
+        text <- read_xml('<text />')
+        xml_attr(text, 'id') <- 'zh-svg'
+        xml_attr(text, 'x') <- (- label_margin) %>% as.character
+        xml_attr(text, 'y') <- (-(i - 1 + 0.5) * pixel_width_with_text) %>% as.character
+        xml_attr(text, 'class') <- 'zh-label'
+        xml_text(text) <- colnames[i]
+        xml_add_child(g_colnames, text)
+      }
+    }
+    xml_add_child(svg, g_colnames)
+
+    
+    msg <- list()
+    msg$svg <- svg %>% as.character
+    #msg$base64 <- heatmap_64
+    #msg$rownames <- rownames(mat)
+    #msg$colnames <- colnames(mat)
+    #msg$nrow <- nrow(mat)
+    #msg$ncol <- ncol(mat)
+    #msg$display_rownames <- display_rownames
+    #msg$display_colnames <- display_colnames
+    #msg$pixel_width <- pixel_width
+    #msg$pixel_height <- pixel_height
+    
+    sizing_policy <- sizingPolicy(padding=0, browser.fill=T, viewer.suppress=!use_viewer)
+    createWidget('zh', msg, sizingPolicy=sizing_policy)
+  }
 
 # asdfasdf ----------------------------------------------------------------
 
--- inst/htmlwidgets/zh/ZH.js
+++ inst/htmlwidgets/zh/ZH.js
@@ -12,62 +12,65 @@ ZH.ZH = function(el_) {
   _container.id = 'zh-container';
   el_.appendChild(_container);
   
-  var _svg = SVG(_container);
-  _svg.node.id = 'zh-svg';
-  
-  var _heatmap;
-  var _rowlabels;
-  var _collabels;
+  //var _svg = SVG(_container);
+  //_svg.node.id = 'zh-svg';
+  //
+  //var _heatmap;
+  //var _rowlabels;
+  //var _collabels;
   
   var _plot = function(msg_) {
-    let pixel_height = msg_.pixel_height;;
-    let pixel_width = msg_.pixel_width;;
-    
-    if (msg_.display_colnames && typeof msg_.colnames !== 'undefined') {
-      _collabels = _svg.group().addClass('zh-rotate');
-      for (let i in msg_.colnames) {
-        let n = msg_.colnames[i];
-        let collabel = _collabels.plain(n)
-                                 .attr('x', - ZH.LABEL_MARGIN)
-                                 .attr('y', - (parseInt(i) + 0.5) * ZH.PIXEL_WIDTH_WITH_TEXT)
-                                 .attr('font-family', null)
-                                 .addClass('zh-label');
-      }
-      pixel_width = ZH.PIXEL_WIDTH_WITH_TEXT;
-    }
-    
-    if (msg_.display_rownames && typeof msg_.rownames !== 'undefined') {
-      _rowlabels = _svg.group();
-      for (let i in msg_.rownames) {
-        let n = msg_.rownames[i];
-        let rowlabel = _rowlabels.plain(n)
-                                 .attr('x', -ZH.LABEL_MARGIN)
-                                 .attr('y', (parseInt(i) + 0.5) * ZH.PIXEL_HEIGHT_WITH_TEXT)
-                                 .attr('font-family', null)
-                                 .addClass('zh-label');
-      }
-      pixel_height = ZH.PIXEL_HEIGHT_WITH_TEXT;
-    }
-    
-    _heatmap = _svg.image()
-                   .attr('x', 0)
-                   .attr('y', 0)
-                   .attr('width', msg_.ncol * pixel_width)
-                   .attr('height', msg_.nrow * pixel_height)
-                   .attr('href', msg_.base64)
-                   .attr('preserveAspectRatio', 'none')
-                   .attr('image-rendering', 'pixelated');
+    _container.innerHTML = msg_.svg;
+    //let pixel_height = msg_.pixel_height;;
+    //let pixel_width = msg_.pixel_width;;
+    //
+    //if (msg_.display_colnames && typeof msg_.colnames !== 'undefined') {
+    //  _collabels = _svg.group().addClass('zh-rotate');
+    //  for (let i in msg_.colnames) {
+    //    let n = msg_.colnames[i];
+    //    let collabel = _collabels.plain(n)
+    //                             .attr('x', - ZH.LABEL_MARGIN)
+    //                             .attr('y', - (parseInt(i) + 0.5) * ZH.PIXEL_WIDTH_WITH_TEXT)
+    //                             .attr('font-family', null)
+    //                             .addClass('zh-label');
+    //  }
+    //  pixel_width = ZH.PIXEL_WIDTH_WITH_TEXT;
+    //}
+    //
+    //if (msg_.display_rownames && typeof msg_.rownames !== 'undefined') {
+    //  _rowlabels = _svg.group();
+    //  for (let i in msg_.rownames) {
+    //    let n = msg_.rownames[i];
+    //    let rowlabel = _rowlabels.plain(n)
+    //                             .attr('x', -ZH.LABEL_MARGIN)
+    //                             .attr('y', (parseInt(i) + 0.5) * ZH.PIXEL_HEIGHT_WITH_TEXT)
+    //                             .attr('font-family', null)
+    //                             .addClass('zh-label');
+    //  }
+    //  pixel_height = ZH.PIXEL_HEIGHT_WITH_TEXT;
+    //}
+    //
+    //_heatmap = _svg.image()
+    //               .attr('x', 0)
+    //               .attr('y', 0)
+    //               .attr('width', msg_.ncol * pixel_width)
+    //               .attr('height', msg_.nrow * pixel_height)
+    //               .attr('href', msg_.base64)
+    //               .attr('preserveAspectRatio', 'none')
+    //               .attr('image-rendering', 'pixelated');
 
-    _svg.rect();
+    //_svg.rect();
     
     // aways crop the svg to the bounding box plus a margin
-    let bbox = _svg.node.getBBox();
+    let svg = document.getElementById('zh-svg');
+    let bbox = svg.getBBox();
     bbox.x -= ZH.MARGIN;
     bbox.y -= ZH.MARGIN;
     bbox.width += 2 * ZH.MARGIN;
     bbox.height += 2 * ZH.MARGIN;
-    _svg.viewbox(bbox);
-    _svg.size(bbox.width, bbox.height);
+    svg.setAttribute('viewBox', [bbox.x, bbox.y, bbox.width, bbox.height].join(' '));
+    svg.setAttribute('width', bbox.width);
+    svg.setAttribute('height', bbox.height);
   };
   this.plot = _plot;
 };
